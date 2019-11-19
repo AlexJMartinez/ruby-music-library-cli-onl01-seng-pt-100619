79d62b78b5fa2734563f1a4a8dfdf46ea20e4bbe
Alex Martinez <alexandr3martin3z@gmail.com>
2019-11-19 06:19:54 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -44,7 +44,9 @@ class MusicLibraryController
     input = gets.strip
 
     if artist = Artist.find_by_name(input)
-    artist.songs.sort
+    artist.songs.sort {|a,b| a.name <=> b.name}.each.with_index(1) do |s,i|
+      puts "{i}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
+    end
   end
 
   def list_songs_by_genre
