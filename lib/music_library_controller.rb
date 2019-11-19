3ff0edcf0c09e8f9c1b8feecb14a76b746ff77e3
Alex Martinez <alexandr3martin3z@gmail.com>
2019-11-19 06:20:21 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -47,7 +47,7 @@ class MusicLibraryController
     artist.songs.sort {|a,b| a.name <=> b.name}.each.with_index(1) do |s,i|
       puts "{i}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
     end
-  end
+   end
   end
 
   def list_songs_by_genre
